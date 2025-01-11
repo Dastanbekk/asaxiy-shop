74c84ec1b4c2c15e1f03b9c040fd0c984890f3d3
DostonbekZaripov <dastandev0110@gmail.com>
2025-01-11 17:19:33 +0500

    bag togirlandi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./js/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}","./src/**/**/*.{html,js}", "./js/**/*.{html,js}"],
   theme: {
     extend: {},
   },
